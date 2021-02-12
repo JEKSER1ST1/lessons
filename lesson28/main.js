deb6f517b47785667ad7f04416ac30f28a77a497
JEKSER1ST1 <74066008+JEKSER1ST1@users.noreply.github.com>
2021-02-12 18:52:10 +0200

    Add files via upload

---
+++ lesson28/main.js
@@ -0,0 +1,58 @@
+let z = prompt ("Привет")
+let x = prompt ("Как тебя зовует ?")
+let X = ("Как тебя зовут?")
+alert ("Привет," + x )
+let c = prompt (" сколько тебе лет?")
+let v = prompt ( "Чем ти увлекаешься"  )
+
+let b = prompt (" Связано ли  это с твоей работой ?")
+let n = prompt (" Тебе нравиться твоя работа ?")
+let m = prompt ( "Сколько ты уделяешь своей работе время?")
+let a = prompt (" Хотел бы ты что-то изменить в свое работе ? ")
+let s = prompt (" Ты хорошо заробатываешь  ?")
+
+let f = prompt (" Сколько бы ты хотел зарабатывать ? ")
+let d = prompt ( "Ты любишь спать ?" )
+
+let g = prompt (" Хотел бы ты поменять что-то в своей жизни ? ")
+let h = prompt (" Что бы ты хотле извемнить в себе ? ")
+let j = prompt (" Хотел бы ты поменять страну ? ")
+let k = prompt (" Какой товй родной город ?")
+let l = prompt (" Хотел бы ты родиться в другом месте ? ")
+let q = prompt (" Какая у тебя мечта в жизни ? ")
+let w = prompt (" Ответ - *Виспаться* не считаеться "  )
+let e = prompt (" Приятого вечера ) ")
+
+
+
+
+
+
+
+
+
+console.log(z)
+console.log(x)
+console.log(X)
+console.log(c)
+console.log(v)
+console.log(b)
+console.log(n)
+console.log(m)
+console.log(a)
+console.log(s)
+console.log(d)
+
+console.log(f)
+console.log(g)
+console.log(h)
+console.log(j)
+console.log(k)
+console.log(l)
+console.log(q)
+console.log(w)
+console.log(e)
+
+
+console.log()
+
