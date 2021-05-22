fd6f91302ca90becf6f45371d0c569344a0d98cf
JEKSER1ST1 <74066008+JEKSER1ST1@users.noreply.github.com>
2021-05-22 23:17:20 +0300

    Add files via upload

---
+++ lesson31/main.js
@@ -0,0 +1,17 @@
+let x = prompt( "Ввелите первое число" )
+let y = prompt( "Введите второе число" )
+alert("Проходит процес")
+console.log( x + y )
+console.log( x - y )
+console.log( x * y )
+let z = x+y
+    e = x-y
+    r = x*y
+    t = x**y
+    u = x%y
+
+alert( "Ваше + число " + z )
+alert( "Ваше - число " + e  )
+alert( "Ваше * число " + r )
+alert( "Ваше ** число " + t )
+alert( "Ваше %  число " + u )
