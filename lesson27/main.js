209d25d1e6a34df8a5041581f920cf04874fa665
JEKSER1ST1 <74066008+JEKSER1ST1@users.noreply.github.com>
2021-05-22 23:18:04 +0300

    Add files via upload

---
+++ lesson27/main.js
@@ -0,0 +1,34 @@
+// let x
+// let y
+// let z
+
+// let x = 13
+//     y = 126
+//     z = 136
+//     c = x+y+z
+//     n = x-y-z
+//     v = x*y*z
+//     r = x/y/z
+//     console.log(c)
+//     console.log(n)
+//     console.log(v)
+//     console.log(r)
+// document.write(c)
+
+// alert ("Жека - го в пабг !?")
+// let x = prompt(" Как тебя зовут")
+// alert ("Привет, " + x )
+
+
+let z = prompt("как тебя зовут")
+alert ("Привет, " + z )
+let x = prompt("Сколько тебе лет")
+alert ("Спасибо" + x )
+let c = prompt("Где ти заботаешь")
+alert ("Ха-ха , прикольно" )
+let v = prompt("Ти любишь кофе") 
+let b = prompt ("Ти любтшь чай")
+
+alert ("Можно к тебе зайти?" + v/b)
+
+// alert ("Привет, " + z + x + c + v )
