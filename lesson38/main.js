33588a94d09945e76657c44312b333617c918bdb
JEKSER1ST1 <74066008+JEKSER1ST1@users.noreply.github.com>
2021-05-22 23:21:58 +0300

    Add files via upload

---
+++ lesson38/main.js
@@ -0,0 +1,77 @@
+// let a = Number  (prompt("Введите число"))
+// if (x == "0"){
+//     alert ( x + 1  )
+// }
+
+// \
+
+// let a = Number  (prompt("Введите число"))
+// if (x == "0"){
+//     alert ( x + 1  )
+// }
+// else (
+//     alert ( x - 2 )
+// )
+// \
+
+// let a = Number(prompt("Введите число"))
+//     b = ( "Введите число" )
+//     z = ("Введите число")
+
+
+// if ( a > 0 && b > 0 && z > 0 ){
+//     alert ("3 wicla vernux")
+// }
+// else if ( a > 0 && b > 0 && z < 0 ) {
+//     alert("Dva wernux wicls")
+
+// }
+// else if ( a > 0 && b < 0 && z > 0 ) {
+//     alert ("Dva wernux wicla")
+// }
+// else if ( a < 0 && b > 0 && z > 0 ) {
+//     alert ("Dva wernux wicls")
+// }
+
+// else if ( a > 0 && b < 0 && z < 0 ){
+//     alert(" 1 правильное  ")
+// }
+// else if ( a < 0 && b < 0 && z > 0 ) {
+//     alert(" 1 пральное ")
+// }
+// else if ( a > 0 && b < 0 && z > 0 ) {
+//     alert(" 1 правильное ")
+// }
+
+// else if ( a < 0 && b > 0 && z < 0 ) {
+//     alert ("1 правильних")
+// }
+
+// else if ( a < 0 && b < 0 && z < 0) {
+//     alert(" 0 правильних ")
+// }
+
+// \
+
+// let x = "Введите число"
+//     z = "Введите число"
+// if ( x > z ) {
+//     alert(x)
+// }
+// else if ( x < z ) {
+//     alert (z)
+// }
+
+
+// \
+
+
+// let a = Number(prompt("Введите число"))
+// if (a == 3){
+//     alert ( a + 1)
+// }
+
+
+
+
+
