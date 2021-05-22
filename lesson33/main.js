15cf77d771f7a1788eaf0b156640a090be5c356e
JEKSER1ST1 <74066008+JEKSER1ST1@users.noreply.github.com>
2021-05-22 23:21:32 +0300

    Add files via upload

---
+++ lesson33/main.js
@@ -0,0 +1,125 @@
+// let boll = 1 || 1
+// alert (boll)
+
+// boll = 0 || 1 
+// alert (boll)
+
+// boll = 1 || 0 
+// alert (boll)
+
+// boll = 0 || 0 
+// alert (boll)
+
+
+
+
+// let z = prompt("Магазин работает")
+
+// if (z < 10 || z > 18)
+//     alert("Магазир не работает")
+// else {
+//     alert("Магазин работает")
+// }
+
+
+
+
+// let boll = 1 && 1
+// alert (boll)
+
+// boll = 0 && 1 
+// alert (boll)
+
+// boll = 1 && 0 
+// alert (boll)
+
+// boll = 0 && 0 
+// alert (boll)
+
+
+
+// alert(1 && -1)
+// alert(1 && 5)
+// alert(null || 5)
+
+// alert (1 && "Саша")
+
+// alert (1 && 2 && null && 3)
+
+// alert (1 && 2 && "Саша")
+
+
+// let x = !1
+// alert (x)
+
+
+// alert (!"Ден")
+
+// alert (!!null)
+
+
+// alert (alert(1) || 2 || alert(3) )
+
+// alert (alert(1) && 2 && alert(3) )
+
+
+
+// alert (null || 2 && 3 || 4)
+
+
+
+// let x = prompt("Кто?")
+//     m = Админ
+//     с = Отмена 
+//     v = Другое
+// if (с){
+//     alert("Ваши действаия не зарегестрировани")
+// }
+// if (v) {
+//     alert("Я вас не знаю")
+// }
+// if (m){
+//     let z = prompt("Ddtlbnt gfhjkm")
+
+//     if (z ! 11)
+//     alert("Действия")
+// else if{
+//     alert("Магазин работает")
+// }
+
+
+
+
+//     console.log(b)
+// }
+
+
+
+let a = prompt ("Кто ты")
+if ( a == "отмена")
+    alert("Ваши действаия не зарегестрировани")
+else if (a == "Другое")
+    alert("Я вас не знаю")
+else( a == "Админ")
+    
+    let password = prompt ("Пароль")
+    if (password == "21")
+        alert("Отмена")
+    else if (password == "22")
+        alert("Неверный пароль")
+    else if (password == "23")
+        alert("Приветсвую")
+    else alert("Пароль не правильный ")
+
+// switch(m){
+//     case '20': alert('Неверный пароль')
+//         break;
+//     case '21': alert("Дейстия отменены")
+//         break;
+//     case '23': alert("Приветствую вас")
+//         break;
+//     default: alert( "Ты ошибся")
+// }
+
+    console.log(m)
+
---
+++ lesson36/main.js
@@ -0,0 +1,57 @@
+// let i = 0
+// while( i < 3) {
+//     alert(i)
+//     i++}
+
+
+// let i = 3
+// while(i) {
+//     alert(i)
+//     i--}
+
+
+
+
+// let i = 0
+// while(i <= 50) {
+//     console.log(i)}
+
+
+
+// let i = 0 
+
+// do {
+//     alert(i)
+//     i++}
+// while (i < 101 ){
+//     console.log(1)}
+
+
+
+// let i = 0
+// for ( ; ; ) {
+//     alert (i)
+// }
+
+
+// for(i=0;i<11;i++){
+//     if(i% 2 == 0 ){
+//         console.log(i)
+// }
+// }
+
+
+
+// let i = 0
+// while( i < 3) {
+//     alert(i)
+//     i++}
+
+
+let arr =[]
+arr[0]=1
+arr[1]=2
+arr[2]=3
+arr[3]=4
+arr[4]=5
+alert(arr)
