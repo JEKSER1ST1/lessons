193c1f13ed2950107f8e6657437c0bbb1bff8b76
JEKSER1ST1 <74066008+JEKSER1ST1@users.noreply.github.com>
2021-05-22 23:16:59 +0300

    Add files via upload

---
+++ lesson29/main.js
@@ -0,0 +1,46 @@
+let z = 5
+let x = 6
+    c = "computer"
+
+// console.log(c)
+// console.log(v)
+// console.log(b)
+// console.log(n)
+// console.log(m)
+// console.log(a)
+
+
+// z = x+5
+// console.log(z)
+// z = z+88
+// console.log(z)
+// z +=44
+// console.log(z)
+ 
+// x /=7
+// x = x/7
+
+
+// z++
+// console.log(z)
+
+// console.log(z ==x)
+// console.log(z>x)
+// console.log(z<x)
+// console.log (z !=x )
+// console.log (z => x)    
+// console.log (z <= x)
+
+console.log(c ==x)
+console.log(c>x)
+console.log(c<x)
+console.log (c !=x )
+console.log (c => x)    
+console.log (c <= x)
+
+console.log(z ==c)
+console.log(z>c)
+console.log(z<c)
+console.log (z !=c )
+console.log (z => c)    
+console.log (z <= c)
